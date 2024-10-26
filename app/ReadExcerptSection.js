82ae69d2819a7d3ff6d5aff736a6f321362b4bac
BodiAugustus <tristanettles@gmail.com>
2024-10-26 14:43:15 -0500

    Replaced PDF Viewer

--- app/ReadExcerptSection.js
+++ app/ReadExcerptSection.js
@@ -1,27 +1,13 @@
 "use client"; // Use client-side rendering
 import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import "@cyntler/react-doc-viewer/dist/index.css"; // Required styles
+import { Cinzel } from "next/font/google";
 
-// Configure the worker for pdf.js
-
-import "react-pdf/dist/Page/TextLayer.css"; // Import text layer styles
-import "react-pdf/dist/Page/AnnotationLayer.css"; // Import annotation layer styles
-import { Cinzel, Playfair_Display } from "next/font/google";
 const cinzel = Cinzel({ subsets: ["latin"] });
-const playfair = Playfair_Display({ subsets: ["latin"] });
-
-// Configure the worker for pdf.js
-if (typeof window !== "undefined") {
-  pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@2.10.377/build/pdf.worker.min.js`;
-}
 
 const ReadExcerptSection = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
+  const docs = [{ uri: "/WebsiteVersion.pdf" }]; // Ensure the PDF is in the public folder
 
   return (
     <div className="bg-orange-500 p-6 xs:h-auto md:h-[100vh] pb-4">
@@ -29,31 +15,7 @@ const ReadExcerptSection = () => {
         Read an Excerpt:
       </h2>
       <div className="bg-white text-black p-4 rounded-lg shadow-lg max-h-[500px] overflow-y-scroll md:w-[55%] mx-auto">
-        <Document
-          file="/WebsiteVersion.pdf"
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <div className="flex justify-between mt-4">
-          <button
-            disabled={pageNumber <= 1}
-            onClick={() => setPageNumber(pageNumber - 1)}
-            className="bg-gray-300 text-black px-4 py-2 rounded disabled:opacity-50"
-          >
-            Previous
-          </button>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-          <button
-            disabled={pageNumber >= numPages}
-            onClick={() => setPageNumber(pageNumber + 1)}
-            className="bg-gray-300 text-black px-4 py-2 rounded disabled:opacity-50"
-          >
-            Next
-          </button>
-        </div>
+        <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 import HeroSection from "./HeroSection";
 import SynopsisSection from "./SynopsisSection";
 import AtAGlanceSection from "./AtAGlanceSection";
